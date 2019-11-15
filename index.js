26a0b9e3365c8df5388c393720fb344e5690c0e3
Daniel Finnerman <danu95d@hotmail.com>
2019-11-15 14:33:09 +0200

    database update

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 'use strict';
 
 const express = require('express');
-
+const connection = require('./model/db.js');
 const app = express();
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-	res.send('Hallo from node server');
+app.get('/animal', async(req, res) => {
+	try {
+const [results, field] = await connection.query(
+  'SELECT * FROM animal',
+  function(err, results, fields) {
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+	res.json(results);
+  } catch (e) {
+	  console.log(e);
+	  res.send('db error');
+  });
 });
-
 app.get('/demo', (req, res) => {
 	res.send('Hallo from demo');
 });
