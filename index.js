08fcc16c85bad9a0bb1e627f9c7ab7a57dfcf458
Daniel Finnerman <danu95d@hotmail.com>
2019-11-18 13:26:43 +0200

    db search bar

--- index.js
+++ index.js
@@ -6,17 +6,30 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/animal', async(req, res) => {
+app.get('/animals', async(req, res) => {
 	try {
-		const [results, fields] = await connection.query('SELECT * FROM animals');
+		const [results, fields] = await connection.query('SELECT * FROM animal');
 		console.log(results);
 		console.log(fields);
 		res.json(results);
 	} catch(e) {
 		console.log(e);
 		res.send('db error:(');
+	}	
+});
+app.get('/animal', async(req, res) => {
+	console.log(req.query);
+	//res.send(`query parameteres? ${res.query}`);
+	try {
+		const[results] = await connection.query('SELECT * FROM animal WHERE name LIKE ?', [req.query.name]);
+			
+			//;, [req.query.id]);
+		res.json(results);
+	} catch(e) {
+		res.send(`db error ${e}`);
 	}
 });
+		
 
 app.get('/demo', (req, res) => {
 	res.send('Hallo from demo');
