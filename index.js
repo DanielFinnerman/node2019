3469da5573884223bf221a711410485e662090e2
Daniel Finnerman <danu95d@hotmail.com>
2019-11-19 13:33:39 +0200

    db input bars

--- index.js
+++ index.js
@@ -1,40 +1,50 @@
 'use strict';
 
 const express = require('express');
-const connection = require('./model/db.js');
+const connection = require('./model/animals')
 const app = express();
-
+const bodyParser = require('body-parser');
+const port = 3000;
 app.use(express.static('public'));
 
-app.get('/animals', async(req, res) => {
+app.get('/animals', async (req, res) => {
 	try {
-		const [results, fields] = await connection.query('SELECT * FROM animal');
-		console.log(results);
-		console.log(fields);
-		res.json(results);
-	} catch(e) {
+		res.json(await connection.getAll());
+	} catch (e) {
 		console.log(e);
-		res.send('db error:(');
-	}	
-});
-app.get('/animal', async(req, res) => {
-	console.log(req.query);
-	//res.send(`query parameteres? ${res.query}`);
-	try {
-		const[results] = await connection.query('SELECT * FROM animal WHERE name LIKE ?', [req.query.name]);
-			
-			//;, [req.query.id]);
-		res.json(results);
-	} catch(e) {
-		res.send(`db error ${e}`);
+		res.send('db error :(');
 	}
 });
-		
 
-app.get('/demo', (req, res) => {
-	res.send('Hallo from demo');
+app.get('/animal', async (req, res) => {
+	console.log(req.query);
+	try {
+		res.json(await connection.search(req.query.name));
+	} catch(e) {
+		res.send(`db error`);
+	}
 });
 
-app.listen(3000, () => {
-	console.log('server start');
+app.post('/animal', bodyParser.urlencoded({extended: true}), async (req, res) => {
+	console.log(req.body);
+	try {
+		res.json(await connection.insert(req.body.name));
+	} catch (e) {
+		console.log(e);
+		res.send('db error :(');
+	}
+});
+
+app.get('/', (req, res) => {
+	res.send('Hello from Node server!');
+});
+
+app.get('/demo', (req, res) => {
+	//console.log('req:', req);
+	//console.log('res:', res);
+	res.send('Hallo from Demo server!');
+});
+
+app.listen(port, () => {
+	console.log(`Listening from port ${port}`);
 });
